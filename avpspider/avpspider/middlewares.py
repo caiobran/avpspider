a9ff77740e4a52bcdfe6b49fa7a564657869f393
caiobran <caiobran88@gmail.com>
2023-02-25 14:51:45 -0600

    minor change

--- avpspider/avpspider/middlewares.py
+++ avpspider/avpspider/middlewares.py
@@ -7,11 +7,12 @@ from scrapy import signals
 from scrapy.http import HtmlResponse
 
 from selenium import webdriver
-from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.service import Service as ChromeService
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+
+from webdriver_manager.chrome import ChromeDriverManager
 
 # useful for handling different item types with a single interface
 #from itemadapter import is_item, ItemAdapter
